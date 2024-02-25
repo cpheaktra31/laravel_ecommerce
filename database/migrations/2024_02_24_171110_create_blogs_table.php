ff2f59235ac227d0bc02df90b489f95add5ea494
pheaktra002 <pheaktrachhay2@gmail.com>
2024-02-25 13:59:17 +0700

    add blog migration

---
+++ database/migrations/2024_02_24_171110_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_en');
+            $table->string('title_kh');
+            $table->string('short_info_en');
+            $table->string('short_info_kh');
+            $table->text('description_kh');
+            $table->text('description_en');
+            $table->string('featured_image');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
